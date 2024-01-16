7d0bbb55b7814f2818b0ecb94db37dcb3ce9e361
dirgafebrian <dirgafebrian45@gmail.com>
2024-01-16 15:49:09 +0700

    update

--- js-bill/account/logout.js
+++ js-bill/account/logout.js
@@ -8,36 +8,17 @@ const logout = () => {
       showCancelButton: true,
       confirmButtonText: "Benar",
       cancelButtonText: "Tidak",
-    })
-
-    if (isConfirmed.isConfirmed) {
-  
-      try {
-        const response =  fetch(target_url, {
-          method: "DELETE",
-          headers: myHeaders,
-          redirect: "follow",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          icon: "success",
+          text: "Data berhasil dihapus",
+          showConfirmButton: false,
         });
-  
-        if (response.ok) {
-           Swal.fire({
-            icon: "success",
-            text: "Data berhasil dihapus",
-            showConfirmButton: false,
-          })
-          .then((result) => {
-      
         deleteCookie("Authorization");
-        window.location.href = "../login.html";
-      
-    });
-        } else {
-          throw new Error("Request failed with status: " + response.status);
-        }
-      } catch (error) {
-        console.error("Error:", error);
+        window.location.href = "login.html";
       }
-    }
+    });
   };
   
 window.logout = logout;
