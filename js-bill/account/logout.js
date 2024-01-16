f77f0725e41e563b432b96362267bbd0a2a4bdd2
dirgafebrian <dirgafebrian45@gmail.com>
2024-01-16 15:57:03 +0700

    Refactor logout function and add confirmation dialog

--- js-bill/account/logout.js
+++ js-bill/account/logout.js
@@ -1,24 +1,28 @@
 import { deleteCookie } from "https://jscroot.github.io/cookie/croot.js";
 
-const logout = () => {
-    Swal.fire({
-      icon: "question",
-      title: "Konfirmasi",
-      text: "Apakah Anda benar ingin logout?",
-      showCancelButton: true,
-      confirmButtonText: "Benar",
-      cancelButtonText: "Tidak",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire({
+const logout = async () => {
+    const isConfirmed = await Swal.fire({
+        text: "Benarkah anda ingin keluar?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Benar",
+        cancelButtonText: "Tidak",
+    });
+
+    if (isConfirmed.isConfirmed) {
+      if (response.ok) {
+        await Swal.fire({
           icon: "success",
           text: "Data berhasil dihapus",
           showConfirmButton: false,
+        }).then(() => {
+          deleteCookie("Authorization");
+          window.location.href = "../index.html";
         });
-        deleteCookie("Authorization");
-        window.location.href = "login.html";
       }
-    });
-  };
+    }
+};
   
 window.logout = logout;
