dd4559440b72f0f29dd80268140e77ab76980719
Fedhira Syaila <pedirasyaila@gmail.com>
2024-01-16 15:45:45 +0700

    update

--- js-bill/account/logout.js
+++ js-bill/account/logout.js
@@ -8,12 +8,36 @@ const logout = () => {
       showCancelButton: true,
       confirmButtonText: "Benar",
       cancelButtonText: "Tidak",
-    }).then((result) => {
-      if (result.isConfirmed) {
+    })
+
+    if (isConfirmed.isConfirmed) {
+  
+      try {
+        const response =  fetch(target_url, {
+          method: "DELETE",
+          headers: myHeaders,
+          redirect: "follow",
+        });
+  
+        if (response.ok) {
+           Swal.fire({
+            icon: "success",
+            text: "Data berhasil dihapus",
+            showConfirmButton: false,
+          })
+          .then((result) => {
+      
         deleteCookie("Authorization");
         window.location.href = "../login.html";
-      }
+      
     });
+        } else {
+          throw new Error("Request failed with status: " + response.status);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
   };
   
 window.logout = logout;
