78d9aa81704d0f43bec81dd5a70c19dee6fdd8af
Fedhira Syaila <pedirasyaila@gmail.com>
2024-01-23 20:48:32 +0700

    update

--- js-bill/sewa/create-sewa.js
+++ js-bill/sewa/create-sewa.js
@@ -21,6 +21,26 @@ const insertSewa = () => {
     insertWithToken(target_url, formData, responseData);
 }
 
+const getCurrentDate = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = (today.getMonth() + 1).toString().padStart(2, '0');
+    const day = today.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+};
+
+document.getElementById("tanggal_mulai").value = getCurrentDate(); // Set Tanggal Mulai to current date
+document.getElementById("tanggal_mulai").min = getCurrentDate(); // Tanggal Mulai cannot be before today
+
+document.getElementById("tanggal_selesai").value = getCurrentDate(); // Set Tanggal Selesai to current date
+document.getElementById("tanggal_selesai").min = getCurrentDate(); // Tanggal Selesai cannot be before today
+
+// Add event listener to Tanggal Mulai to update Tanggal Selesai min attribute
+document.getElementById("tanggal_mulai").addEventListener("change", () => {
+    const startDate = document.getElementById("tanggal_mulai").value;
+    document.getElementById("tanggal_selesai").min = startDate;
+});
+
 const responseData = (result) => {
     if (result.status === 201) {
         Swal.fire({
