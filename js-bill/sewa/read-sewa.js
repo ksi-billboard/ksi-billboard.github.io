f6cddee51ab6953ffc3286a403a9f335aa228cda
dirgafebrian <dirgafebrian45@gmail.com>
2024-01-25 01:41:46 +0700

    Add tableSewaBayar template and conditionally render it based on value.bayar

--- js-bill/sewa/read-sewa.js
+++ js-bill/sewa/read-sewa.js
@@ -1,22 +1,33 @@
 import { addInner } from "https://jscroot.github.io/element/croot.js";
 import { getWithToken } from "../temp/component.js";
-import { tableSewa } from "../temp/table.js";
+import { tableSewa, tableSewaBayar } from "../temp/table.js";
 
 const target_url = "https://asia-southeast2-keamanansistem.cloudfunctions.net/sewa"
 
 const dataSewa  = (value) => {
-    const data = tableSewa
-    .replace("#GAMBAR#", value.content)
-    .replace("#MULAI#", value.tanggal_mulai)
-    .replace("#SELESAI#", value.tanggal_selesai)
-    .replace("#ID#", value._id)
-    .replace("#IDEDIT#", value._id)
-    .replace("#HAPUS#", value._id)
-    .replace("#ID#", value._id);
+    if(value.bayar) {
+        const data = tableSewaBayar
+        .replace("#GAMBAR#", value.content)
+        .replace("#MULAI#", value.tanggal_mulai)
+        .replace("#SELESAI#", value.tanggal_selesai)
+        .replace("#ID#", value._id)
+        .replace("#IDEDIT#", value._id)
+        .replace("#HAPUS#", value._id)
+        // .replace("#ID#", value._id);
 
-    addInner("tableAllSewa", data);
+        addInner("tableAllSewa", data);
+    } else {
+        const data = tableSewa
+        .replace("#GAMBAR#", value.content)
+        .replace("#MULAI#", value.tanggal_mulai)
+        .replace("#SELESAI#", value.tanggal_selesai)
+        .replace("#ID#", value._id)
+        .replace("#IDEDIT#", value._id)
+        .replace("#HAPUS#", value._id)
+        .replace("#ID#", value._id);
 
-    console.log(value);
+        addInner("tableAllSewa", data);
+    }
 }
 
 const responseData = (result) => {
